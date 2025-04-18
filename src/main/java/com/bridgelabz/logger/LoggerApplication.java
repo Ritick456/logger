6038ef9fc4ef468a53fd5261644c3777ca419502
Ritick456 <ritickkr937@gmail.com>
2025-04-18 14:30:25 +0530

    [Ritick]add: created logger file

--- src/main/java/com/bridgelabz/logger/LoggerApplication.java
+++ src/main/java/com/bridgelabz/logger/LoggerApplication.java
@@ -1,13 +1,28 @@
 package com.bridgelabz.logger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LoggerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggerApplication.class, args);
-	}
+	// Using SLF4J Logger
+	public static final Logger logger = LoggerFactory.getLogger(LoggerApplication.class);
 
+	public static void main(String[] args) {
+		// Corrected logger.debug with proper typo fix
+		logger.debug("Welcome to Spring Concept Demo");
+
+		// Run Spring Boot Application
+		ApplicationContext context = SpringApplication.run(LoggerApplication.class, args);
+
+		// Log context bean information
+		logger.debug("Checking Context: {}", context.getBean("demoBean")); // Assuming bean name is 'demoBean'
+
+		// Additional logging examples
+		logger.debug("\n*** Example Using @Autowired annotation on property ***");
+	}
 }
